09fb145829b414534dda37f7c5d66c5df8e957e6
MANISH KAPOOR <128586718+Manishkapoor2002@users.noreply.github.com>
2025-09-05 11:05:17 +0530

    Create tests/pages/EpamHomePage.ts

---
+++ tests/pages/EpamHomePage.ts
@@ -0,0 +1,25 @@
+import { Page } from '@playwright/test';
+
+export class EpamHomePage {
+  private page: Page;
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  async navigateTo() {
+    await this.page.goto('https://www.epam.com/');
+  }
+
+  async clickServicesMenu() {
+    await this.page.click('text=Services');
+  }
+
+  async clickExploreClientWork() {
+    await this.page.click('text=Explore Our Client Work');
+  }
+
+  async isClientWorkTextVisible() {
+    return await this.page.isVisible('text=Client Work');
+  }
+}
